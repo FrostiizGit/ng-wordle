1637c9a6bf5d161f6b6faff1f8fb41e235a753d4
Legrand Valentin <valchti@gmail.com>
2022-01-30 22:04:27 +0100

    Added header & main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { ViewAllPollsComponent } from './components/view-all-polls/view-all-polls.component';
+import { PollCardComponent } from './components/poll-card/poll-card.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NewPollComponent } from './components/new-poll/new-poll.component';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { PollVoteComponent } from './components/poll-vote/poll-vote.component';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { CreatePollComponent } from './components/create-poll/create-poll.component';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ViewAllPollsComponent,
+    PollCardComponent,
+    NewPollComponent,
+    PollVoteComponent,
+    CreatePollComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzCardModule,
+    NzTabsModule,
+    NzProgressModule,
+    NzRadioModule,
+    NzButtonModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzCheckboxModule,
+    NzIconModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
